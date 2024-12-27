d27e69646c3e6c6ef27b0631f305ffe1d8f79a1f
faroukq1 <faroukq1@gmail.com>
2024-12-27 22:53:24 +0100

    add express json

--- department service/app.js
+++ department service/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-
+app.use(express.json());
 app.listen(process.env.PORT, () => {
   console.log("server running on port : " + process.env.PORT);
 });
